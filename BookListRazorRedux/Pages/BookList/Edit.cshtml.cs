29adb3da5d9aaa247ef8b2c1f56c288a39c657df
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-07-21 09:31:48 +0300

    Create and Edit pages and handlers

--- BookListRazorRedux/Pages/BookList/Edit.cshtml.cs
+++ BookListRazorRedux/Pages/BookList/Edit.cshtml.cs
@@ -8,11 +8,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazorRedux.Pages.BookList
 {
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
 
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
@@ -20,21 +20,26 @@ namespace BookListRazorRedux.Pages.BookList
         [BindProperty]
         public Book Book { get; set; }
 
-        public void OnGet()
+        public async Task OnGet(int id)
         {
-
+            Book = await _db.Book.FindAsync(id);
         }
 
         public async Task<IActionResult> OnPost()
         {
             if (ModelState.IsValid)
             {
-                await _db.Book.AddAsync(Book);
+                var bookFromDb = await _db.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.Author = Book.Author;
+                bookFromDb.ISBN = Book.ISBN;
+
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
+
             } else
             {
-                return Page();
+                return RedirectToPage();
             }
         }
     }
